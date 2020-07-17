a366d4e6ac72524658218a45d50435e528049f48
Vic Chimenti <chimenti@seattleu.edu>
2020-07-17 08:23:04 -0700

    make content item name searchable and hidden

--- ucor-course-item.js
+++ ucor-course-item.js
@@ -151,16 +151,16 @@ try {
     } else {
         document.write('<div class="row levelSix courseDetails keyWords" style="display: none"><h5>No Keywords Provided</h5></div>');
     }
-    
+
 
     /* -- Write Program Level 7 Details --*/
-    if (keyWords != "") {
+    if (contentItemName != "") {
         /* -- Keywords is a hidden field that is used to add searchable keywords when creating a program item but is not displayed on the live page -- */
-        document.write('<div class="row col-xs-12 levelSix">');
-        document.write('<div class="col-xs-12 courseDetails keyWords" style="display: none"><h5>Keywords</h5><div class="courseInfo">' + keyWords + '</div></div>');
+        document.write('<div class="row col-xs-12 levelSeven">');
+        document.write('<div class="col-xs-12 courseDetails contentItemName" style="display: none"><h5>Keywords</h5><div class="courseInfo">' + contentItemName + '</div></div>');
         document.write('</div>');
     } else {
-        document.write('<div class="row levelSix courseDetails keyWords" style="display: none"><h5>No Keywords Provided</h5></div>');
+        document.write('<div class="row levelSeven courseDetails contentItemName" style="display: none"><h5>No Content Item Name Provided</h5></div>');
     }
 
 
