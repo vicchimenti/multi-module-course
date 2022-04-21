bae16fa131af28791b311d7548c684172eab59c2
Vic Chimenti <victor.chimenti@gmail.com>
2022-04-21 08:07:57 -0700

    import utilities

--- ucor-course-item.js
+++ ucor-course-item.js
@@ -15,6 +15,217 @@
 */
 
 
+
+
+/***
+ *     @author  Victor Chimenti, MSCS
+ *     @file    ucor-course-item.js
+ *              v9/organizer/ucorCourse
+ *                  id:5424
+ *
+ *     This content type will work in conjunction with the Organizer.
+ *
+ *     Document will write once when the page loads
+ *
+ *     @version 2.6.1
+ */
+
+
+
+
+
+
+
+
+/***
+ *      Import T4 Utilities
+ */
+    importClass(com.terminalfour.media.IMediaManager);
+    importClass(com.terminalfour.spring.ApplicationContextProvider);
+    importClass(com.terminalfour.publish.utils.BrokerUtils);
+    importClass(com.terminalfour.media.utils.ImageInfo);
+
+
+
+
+    /***
+     *      Extract values from T4 element tags
+     *      and confirm valid existing content item field
+     */
+    function getContentValues(tag) {
+        try {
+            let _tag = BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, tag).trim();
+            return {
+                isError: false,
+                content: _tag == '' ? null : _tag
+            }
+        } catch (error) {
+            return {
+                isError: true,
+                message: error.message
+            }
+        }
+    }
+
+
+
+
+    /***
+     *      Returns an array of list items
+     */
+    function assignList(arrayOfValues) {
+
+        let listValues = '';
+
+        for (let i = 0; i < arrayOfValues.length; i++) {
+
+            listValues += '<li class="list-group-item sdgIcon">' + arrayOfValues[i].trim() + '</li>';
+        }
+
+        return listValues;
+    }
+
+
+
+
+    /***
+     *      Returns a media object
+     */
+    function getMediaInfo(mediaID) {
+
+        let mediaManager = ApplicationContextProvider.getBean(IMediaManager);
+        let media = mediaManager.get(mediaID, language);
+
+        return media;
+    }
+
+
+
+
+    /***
+     *      Returns a media stream object
+     */
+    function readMedia(mediaID) {
+
+        let mediaObj = getMediaInfo(mediaID);
+        let oMediaStream = mediaObj.getMedia();
+
+        return oMediaStream;
+    }
+
+
+
+
+    /***
+     *      Returns a formatted html img tag
+     */
+    function mediaTag(itemId) {
+
+        let mediaPath = BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="media" formatter="path/*" id="' + itemId + '" />');
+        let mediaInfo = getMediaInfo(itemId);
+        let media = readMedia(itemId);
+        let info = new ImageInfo;
+        info.setInput(media);
+
+        let mediaHTML = (info.check()) ?
+            '<figure class="figure"><img src="' + mediaPath + '" class="listgroupImage figure-img img-fluid" aria-label="' + mediaInfo.getName() + '" alt="' + mediaInfo.getDescription() + '" width="' + info.getWidth() + '" height="' + info.getHeight() + '" loading="auto" /></figure><figcaption class="figure-caption visually-hidden hidden">' + mediaInfo.getName() + '</figcaption>' :
+            '<span class="listgroupImage visually-hidden hidden">Invalid Image ID</span>';
+
+        return mediaHTML;
+    }
+
+
+
+
+    /***
+     *      Returns a formatted html img tag
+     */
+    function getTarget(itemId) {
+
+        let mediaInfo = getMediaInfo(itemId);
+        let media = readMedia(itemId);
+        let info = new ImageInfo;
+        info.setInput(media);
+
+        let target = (info.check()) ? '' + mediaInfo.getName() + '' : null;
+
+        return target;
+    }
+
+
+
+
+    /***
+     *      Returns an array of list items
+     */
+    function formatTargets(arrayOfValues) {
+
+        let listValues = '';
+
+        for (let i = 0; i < arrayOfValues.length; i++) {
+
+            if (arrayOfValues[i]) {
+                let cleanValue = arrayOfValues[i].replace(/\s/g, '-');
+                listValues += '' + cleanValue.trim() + ' ';
+            }
+        }
+
+        return listValues;
+    }
+
+
+
+
+    /***
+     *      Write the document
+     */
+    function writeDocument(array) {
+
+        for (let i = 0; i < array.length; i++) {
+
+            document.write(array[i]);
+        }
+    }
+
+
+
+
+
+
+
+
+    /***
+     *  Main
+     */
+    try {
+
+
+        /***
+         *      Dictionary of content
+         * */
+        let cejscDict = {
+
+            contentName: getContentValues('<t4 type="content" name="Name" output="normal" modifiers="striptags,htmlentities" />'),
+            articleTitle: getContentValues('<t4 type="content" name="Article Title" output="normal" modifiers="striptags,htmlentities" />'),
+            courseName: getContentValues('<t4 type="content" name="Course Name" output="normal" modifiers="striptags,htmlentities" />'),
+            college: getContentValues('<t4 type="content" name="College" output="normal" modifiers="striptags,htmlentities" />'),
+            academicLevel: getContentValues('<t4 type="content" name="Section Academic Level" output="normal" modifiers="striptags,htmlentities" />'),
+            primarySectionName: getContentValues('<t4 type="content" name="Primary Section Name" output="normal" modifiers="striptags,htmlentities" />'),
+            subjectDescription: getContentValues('<t4 type="content" name="Subject" output="normal" modifiers="striptags,htmlentities" />'),
+            icons: getContentValues('<t4 type="content" name="Icon ID" output="normal" modifiers="striptags,htmlentities" />'),
+            summaryDescription: getContentValues('<t4 type="content" name="Plaintext Description" output="normal" modifiers="striptags,htmlentities" />'),
+            courseDescription: getContentValues('<t4 type="content" name="Description" output="normal" modifiers="medialibrary,nav_sections,htmlentities" />'),
+            fullTextLink: getContentValues('<t4 type="content" name="Article Title" output="fulltext" use-element="true" filename-element="Article Title" modifiers="striptags,htmlentities" />'),
+            contentId: getContentValues('<t4 type="meta" meta="content_id" />')
+
+        };
+
+
+
+
+
+
+
 try {
     /* -- Initialize function scope variables -- */
     
