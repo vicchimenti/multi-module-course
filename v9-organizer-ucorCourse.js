cf6a3e2773ef5ac842d24cd09d9ee30b63ce452c
Vic Chimenti <victor.chimenti@gmail.com>
2022-04-22 08:25:37 -0700

    descriptionWrapper

--- v9-organizer-ucorCourse.js
+++ v9-organizer-ucorCourse.js
@@ -127,6 +127,8 @@ try {
     let closeCardBody = '</div>';
     let openContainer = '<div class="container-fluid">';
     let closeContainer = '</div>';
+    let descriptionWrapper = '<div class="row col-xs-12 fullCourseDescriptionWrapper">';
+    let closeDescriptionWrapper = '</div>';
     let detailsType = '<div class="col-xs-12 courseDetails courseTypes"><h4>Course Type: </h4><span class="courseType">' + ucorDict.courseType.content + '</span></div>';
     let detailsFaculty = '<div class="col-xs-12 col-sm-3 courseDetails instructors"><h5>Faculty: </h5><span class="faculty">' + ucorDict.faculty.content + '</span></div>';
     let detailsTerm = '<div class="col-xs-12 col-sm-3 courseDetails terms"><h5>Term: </h5><span class="term">' + ucorDict.term.content + '</span></div>';
